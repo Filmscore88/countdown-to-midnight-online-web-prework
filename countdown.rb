11a192b0e43cfa3f6fdf56599c291076009d74a6
Alberto Morales <cmtmorales24@gmail.com>
2019-02-24 03:52:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
 
-def countdown
+
+def countdown(num)
+  x=num 
+  while x > 0
+    puts"#{x} SECOND(S)!"
+    x-=1 
+end
+  return "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(num)
+i=num 
+  while i > -1
+    puts"#{i} SECOND(S)!"
+    i-=1 
+    sleep 1 
+end
+  return "HAPPY NEW YEAR!"
+
 end
